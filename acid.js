86c2a48dd6d251a6987b84fe59a5f5b94090aa90
Robert Tod <robert@qubitdigital.com>
2013-09-09 23:44:42 +0100

    Add border and zoom option

--- acid.js
+++ acid.js
@@ -1,39 +1,47 @@
-function makeSwatch (size, swatchSize, top, left) {
+function makeSwatch (size, swatchSize, borderWidth, top, left) {
   var offset = (size * swatchSize) - swatchSize;
   var swatch = document.createElement("div");
   swatch.style.left = (left * swatchSize * 2) - offset + "px";
   swatch.style.top = (top * 2 * swatchSize) - offset + "px";
-  swatch.style.width = swatchSize + "px";
-  swatch.style.height = swatchSize + "px";
+  swatch.style.width = swatchSize - 2 - borderWidth + "px";
+  swatch.style.height = swatchSize - 2 - borderWidth + "px";
+
+  // Required to make right column centered
+  if (left === size - 1) {
+    swatch.style.right = "-" + swatch.style.left;
+    swatch.style.left = "0px";
+  }
 
   if (top === 0) {
     var hue = 0;
-    var lightness = 100 * ((size - left) / size) +"%";
+    var lightness = 100 * ((size - left) / size);
     var saturation = "0%";
   } else {
     var hue = 360 * (left / size);
-    var lightness = 20 + ((70 * (top / size))) + "%";
+    var lightness = 20 + ((70 * (top / size)));
     var saturation = "80%";
   }
 
-  swatch.style.backgroundColor = "hsl(" + hue + ", " + saturation + ", " + lightness + ")";
+  swatch.className = lightness > 70 ? "light" : "dark";
+
+  swatch.style.backgroundColor = "hsl(" + hue + ", " + saturation + ", " + lightness + "%)";
 
   return swatch;
 }
 
-function makePalette (size, swatchSize) {
+function makePalette (size, swatchSize, borderWidth, zoom) {
   var i = 0, j, swatches = [];
 
   var palette = document.createElement("div");
-  palette.className = "palette";
-  palette.style.height = swatchSize * size + "px";
-  palette.style.width = swatchSize * size + "px";
+  palette.className = "palette " + (zoom || "no") + "-zoom";
+  palette.style.height = swatchSize * size + borderWidth + "px";
+  palette.style.width = swatchSize * size + borderWidth + "px";
 
   for (; i < size; i++) {
     j = 0;
     swatches[i] = [];
     for (; j < size; j++) {
-      swatches[i][j] = makeSwatch(size, swatchSize, i, j);
+      swatches[i][j] = makeSwatch(size, swatchSize, borderWidth, i, j);
       palette.appendChild(swatches[i][j]);
     }
   }
@@ -43,5 +51,5 @@ function makePalette (size, swatchSize) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.body.appendChild(makePalette(10, 15));
+  document.body.appendChild(makePalette(10, 15, 1, "low"));
 });
