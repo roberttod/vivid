4715a42d335f31b19b804e9c41e732330f8d8700
Robert Tod <robert@qubitdigital.com>
2013-09-10 00:55:54 +0100

    Make Acid Class

--- acid.js
+++ acid.js
@@ -1,55 +1,97 @@
-function makeSwatch (size, swatchSize, borderWidth, top, left) {
-  var offset = (size * swatchSize) - swatchSize;
-  var swatch = document.createElement("div");
-  swatch.style.left = (left * swatchSize * 2) - offset + "px";
-  swatch.style.top = (top * 2 * swatchSize) - offset + "px";
-  swatch.style.width = swatchSize - 2 - borderWidth + "px";
-  swatch.style.height = swatchSize - 2 - borderWidth + "px";
-
-  // Required to make right column centered
-  if (left === size - 1) {
-    swatch.style.right = "-" + swatch.style.left;
-    swatch.style.left = "0px";
-  }
-
-  if (top === 0) {
-    var hue = 0;
-    var lightness = 100 * ((size - left) / size);
-    var saturation = "0%";
-  } else {
-    var hue = 360 * (left / size);
-    var lightness = 20 + ((70 * (top / size)));
-    var saturation = "80%";
-  }
-
-  swatch.className = lightness > 70 ? "light" : "dark";
-
-  swatch.style.backgroundColor = "hsl(" + hue + ", " + saturation + ", " + lightness + "%)";
-
-  return swatch;
-}
-
-function makePalette (size, swatchSize, borderWidth, zoom) {
-  var i = 0, j, swatches = [];
-
-  var palette = document.createElement("div");
-  palette.className = "palette " + (zoom || "no") + "-zoom";
-  palette.style.height = swatchSize * size + borderWidth + "px";
-  palette.style.width = swatchSize * size + borderWidth + "px";
-
-  for (; i < size; i++) {
-    j = 0;
-    swatches[i] = [];
-    for (; j < size; j++) {
-      swatches[i][j] = makeSwatch(size, swatchSize, borderWidth, i, j);
-      palette.appendChild(swatches[i][j]);
+function defaults(options, defaults) {
+  for (var key in defaults) {
+    if (!(key in options) || options[key] === undefined) {
+      options[key] = defaults[key];
     }
   }
-
-  return palette;
-
+  return options;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.body.appendChild(makePalette(10, 15, 1, "low"));
-});
+function Acid (options) {
+  this.options = options || {};
+  defaults(this.options, {
+    size: 10,
+    swatchSize: 15,
+    zoom: "low",
+    borderWidth: 1
+  });
+  this.initialize();
+}
+
+Acid.prototype = {
+  initialize: function () {
+    this.el = document.createElement("div");
+    return this;
+  },
+  render: function () {
+    this.el.innerHTML = "";
+    this.el.appendChild(this.makePicker());
+    return this;
+  },
+  makeSwatch: function (top, left) {
+    var offset = (this.options.size * this.options.swatchSize) - this.options.swatchSize;
+    var swatch = document.createElement("div");
+    swatch.style.left = (left * this.options.swatchSize * 2) - offset + "px";
+    swatch.style.top = (top * 2 * this.options.swatchSize) - offset + "px";
+    swatch.style.width = this.options.swatchSize - 2 - this.options.borderWidth + "px";
+    swatch.style.height = this.options.swatchSize - 2 - this.options.borderWidth + "px";
+
+    // Required to make right column centered
+    if (left === this.options.size - 1) {
+      swatch.style.right = "-" + swatch.style.left;
+      swatch.style.left = "0px";
+    }
+
+    // The top row is for greys
+    if (top === 0) {
+      var hue = 0;
+      var lightness = 100 * ((this.options.size - left) / this.options.size);
+      var saturation = "0%";
+    } else {
+      var hue = 360 * (left / this.options.size);
+      var lightness = 20 + ((70 * (top / this.options.size)));
+      var saturation = "80%";
+    }
+
+    swatch.className = lightness > 70 ? "light" : "dark";
+
+    swatch.style.backgroundColor = "hsl(" + hue + ", " + saturation + ", " + lightness + "%)";
+
+    swatch.addEventListener("click", function () {
+      this.preview.style.backgroundColor = swatch.style.backgroundColor;
+    }.bind(this));
+
+    return swatch;
+  },
+  makePalette: function () {
+    var i = 0, j, swatches = [];
+
+    var palette = document.createElement("div");
+    palette.className = "palette " + (this.options.zoom || "no") + "-zoom";
+    palette.style.height = this.options.swatchSize * this.options.size - 1 + "px";
+    palette.style.width = this.options.swatchSize * this.options.size - 1 + "px";
+
+    for (; i < this.options.size; i++) {
+      j = 0;
+      swatches[i] = [];
+      for (; j < this.options.size; j++) {
+        swatches[i][j] = this.makeSwatch(i, j);
+        palette.appendChild(swatches[i][j]);
+      }
+    }
+    this.swatches = swatches;
+
+    return palette;
+  },
+  makePicker: function () {
+    this.preview = document.createElement("div");
+    this.preview.className = "preview";
+    var palette = this.makePalette();
+
+    var picker = document.createElement("div");
+    picker.className = "picker";
+    picker.appendChild(this.preview);
+    picker.appendChild(palette);
+    return picker;
+  }
+};
